3fbeb86396a139325233e1343fce8a5254f9a90f
abdallah <abdallhamohamed116@gmail.com>
2023-05-05 14:06:53 +0300

    target interface

---
+++ src/adapter1/Vehicle.java
@@ -0,0 +1,9 @@
+package adapter1;
+//target interface
+//this is shape for eny vehicle if we want it
+public interface Vehicle {
+    void accelerate();
+    void  pushBreak();
+    void soundHorn();
+
+}
